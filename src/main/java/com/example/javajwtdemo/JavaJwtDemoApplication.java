a409a10a1d0a25dd20fbf6635def254f6edf1bb0
Huy Pham <huypt110402@gmail.com>
2024-04-27 13:32:30 +0700

    base

---
+++ src/main/java/com/example/javajwtdemo/JavaJwtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.javajwtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJwtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJwtDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javajwtdemo/JavaJwtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javajwtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJwtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
